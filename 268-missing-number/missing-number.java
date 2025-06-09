88ef1328985c6cefe8ca42901cc00a5b3da76255
Deena <dynamitebutter137@gmail.com>
2025-06-09 14:04:15 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (71.35%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -9,3 +9,17 @@ class Solution {
         return expectedSum - actualSum;
     }
 }
+
+// using XOR
+/* 
+    int allXOR = 0;
+    for(int i=0; i <= nums.length; i++) {
+        allXOR = allXOR ^ i;
+    }
+
+    for(int num : nums){
+        allXOR = allXOR ^ num;
+    }
+    return allXOR;
+
+*/
