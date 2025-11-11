a5ed6d32ddefca2aebae78bf3c95c575657c5078
Deena <dynamitebutter137@gmail.com>
2025-11-11 15:05:47 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (91.97%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy_price = prices[0];
+        int tot_profit = 0;
+
+        for(int i=1; i<prices.length; i++){
+            if(prices[i] < buy_price) {
+                buy_price = prices[i];
+            } else {
+                int curr_profit = prices[i] - buy_price;
+                tot_profit += curr_profit;
+                buy_price = prices[i];
+            }
+        }
+        return tot_profit;
+    }
+}
