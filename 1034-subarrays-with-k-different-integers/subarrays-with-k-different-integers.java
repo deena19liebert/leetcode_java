94e697b83ce8b3e210d5a95951bd345b6fb16ed8
Deena <dynamitebutter137@gmail.com>
2025-11-28 13:30:30 +0530

    Time: 6 ms (94.65%) | Memory: 49.5 MB (28.67%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -3,20 +3,31 @@ class Solution {
         return atMost(nums, k) - atMost(nums, k-1);
     }
     private int atMost(int[] nums, int x) {
-        Map<Integer, Integer> map = new HashMap<>();
+        // Map<Integer, Integer> map = new HashMap<>();
 
-        int count=0, left=0;
+        int count=0, left=0, distinct=0 ;
+        int[] freq = new int[nums.length + 1];
+
         for(int right=0; right < nums.length; right++){
-            int r = nums[right];
-            map.put(r, map.getOrDefault(r, 0) + 1);
-            while(map.size() > x) {
-                int l = nums[left];
-                map.put(l, map.getOrDefault(l, 0) - 1);
-                if(map.get(l) == 0){
-                    map.remove(l);
-                }
+            if (freq[nums[right]] == 0) distinct++;
+            freq[nums[right]]++;
+
+            //int r = nums[right];
+            //map.put(r, map.getOrDefault(r, 0) + 1);
+
+            while (distinct > x) {
+                freq[nums[left]]--;
+                if (freq[nums[left]] == 0) distinct--;
                 left++;
             }
+
+            // while(map.size() > x) {
+            //     int l = nums[left];
+            //     map.put(l, map.getOrDefault(l, 0) - 1);
+            //     if(map.get(l) == 0){
+            //         map.remove(l);
+            //     }
+            //     left++; }
             count += right - left + 1;
         }
         return count;
