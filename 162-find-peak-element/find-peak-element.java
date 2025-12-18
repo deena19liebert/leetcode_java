d0b67c3c6e3c69230174238f6bc46e8d2c1a0476
Deena <dynamitebutter137@gmail.com>
2025-12-18 21:33:55 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (7.14%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,19 +1,16 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-
-    // think about it, BINARY SEARCH MAKES A LOT OF SENSE HERE !
-        int left = 0;
-        int right = nums.length - 1;
-
-        while(left < right) {
-            int mid = (left + right) / 2;
-            if(nums[mid] > nums[mid+1]) {
-                right = mid;
-            }
-            else {
-                left = mid + 1;
-            }
+        int n = nums.length;
+        if(nums.length == 1) return 0;
+        if(nums[0] > nums[1]) return 0;
+        if(nums[n-1] > nums[n-2]) return n-1;
+        int low = 1, high = n-1;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+             if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) return mid;
+             else if(nums[mid] > nums[mid-1]) low = mid+1;
+             else high = mid - 1;
         }
-        return left;
+        return -1;
     }
 }
