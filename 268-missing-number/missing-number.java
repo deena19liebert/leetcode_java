8d66aff14195803ddc03fd0b499e8b5ee43e5b4f
Deena <dynamitebutter137@gmail.com>
2025-12-29 11:12:43 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (29.02%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,12 +1,25 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for(int i=0; i<=nums.length; i++){
-            set.add(i);
+        int totSum = 0, sum = 0;
+        for(int i=0; i <= nums.length; i++){
+            totSum += i;
         }
         for(int num : nums){
-            set.remove(num);
+            sum += num;
         }
-        return set.iterator().next();
+        return totSum - sum;
     }
 }
+
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         Set<Integer> set = new HashSet<>();
+//         for(int i=0; i<=nums.length; i++){
+//             set.add(i);
+//         }
+//         for(int num : nums){
+//             set.remove(num);
+//         }
+//         return set.iterator().next();
+//     }
+// }
