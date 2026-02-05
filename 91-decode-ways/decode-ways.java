eda62bb6d74bb3e56f34035cc6860bbf33d35983
Deena <dynamitebutter137@gmail.com>
2026-02-05 12:34:28 +0530

    Time: 1 ms (85.16%) | Memory: 43.6 MB (9.31%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -7,6 +7,7 @@ class Solution {
         dp[1] = s.charAt(0) == '0' ? 0 : 1;
 
         for(int i=2; i<=n; i++){
+            //Integer.valueOf -> converts integer to string i.e     "1" -> 1
             int oneDigit = Integer.valueOf(s.substring(i-1, i));
             int twoDigit = Integer.valueOf(s.substring(i-2, i));
 
