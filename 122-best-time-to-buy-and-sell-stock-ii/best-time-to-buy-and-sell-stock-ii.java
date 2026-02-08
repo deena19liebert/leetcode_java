ba2bea812f0e5557399600e6592c44a546278c88
Deena <dynamitebutter137@gmail.com>
2026-02-08 14:22:50 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (24.69%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,17 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int buy_price = prices[0];
-        int tot_profit = 0;
-
+        // Greedy Approach Works Here!
+        int profit = 0;
+        int sum = 0;
         for(int i=1; i<prices.length; i++){
-            if(prices[i] < buy_price) {
-                buy_price = prices[i];
-            } else {
-                int curr_profit = prices[i] - buy_price;
-                tot_profit += curr_profit;
-                buy_price = prices[i];
+            sum = prices[i] - prices[i-1];
+            if(sum > 0){
+                profit += sum;
             }
         }
-        return tot_profit;
+        return profit;
     }
 }
