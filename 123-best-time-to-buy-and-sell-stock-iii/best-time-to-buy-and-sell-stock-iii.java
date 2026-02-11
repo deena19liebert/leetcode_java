dba6960014e2f090b5fedc9d355ff00986a8440d
Deena <dynamitebutter137@gmail.com>
2026-02-11 11:56:48 +0530

    Time: 1 ms (99.45%) | Memory: 75.8 MB (80.21%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,3 +1,21 @@
+// class Solution {  //RECURSION: TC = O(N^2), SC = O(N)
+//     public static int f(int i, int canBuy, int transactionLeft, int[] prices){
+//         if(transactionLeft == 0 || i==prices.length) return 0;
+//         if(canBuy == 1){
+//             int buy = -prices[i] + f(i+1, 0, transactionLeft, prices);
+//             int notBuy = 0 + f(i+1, 1, transactionLeft, prices);
+//             return Math.max(buy, notBuy);
+//         } else {
+//             int sell = prices[i] + f(i+1, 1, transactionLeft-1, prices);
+//             int notSell = 0 + f(i+1, 0, transactionLeft, prices);
+//             return Math.max(sell, notSell);
+//         }
+//     }
+//     public int maxProfit(int[] prices) {
+//         return f(0, 1, 2, prices); //transactionLeft = 2, bcs you hv to complete atmost 2 transactions
+//     }
+// }     //OPTIMISED VERSION
+
 class Solution {
     public int maxProfit(int[] prices) {
         if(prices == null || prices.length == 0) return 0;
