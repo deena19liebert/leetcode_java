05a0aef7faad76b1719858e7ca27b915a1d0bc89
Deena <dynamitebutter137@gmail.com>
2026-02-11 13:26:05 +0530

    Time: 1 ms (87.93%) | Memory: 44.4 MB (36.27%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,19 +1,20 @@
-// TIME COMPLEXITY: O[N(2^N)]  SPACE COMPLEXITY: O(N)
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> resultList = new ArrayList<>();
-        //BACKTRACKING FROM THE BEGINNING
-        backtrack(resultList, new ArrayList<>(), nums, 0);
-        return resultList;
-    }
-    private void backtrack(List<List<Integer>> resultSets, List<Integer> tempSet, int[] nums, int start) {
-        resultSets.add(new ArrayList<>(tempSet));
-        for(int i=start; i<nums.length; i++){
-            tempSet.add(nums[i]);
-            backtrack(resultSets, tempSet, nums, i+1);
-            tempSet.remove(tempSet.size() - 1);
+        public static void backtrack(int i, List<List<Integer>> result, List<Integer> subset, int[] nums){
+        if(i == nums.length) {
+            result.add(new ArrayList<>(subset));
+            return;
         }
+        // INCLUDE nums[i]
+        subset.add(nums[i]);
+        backtrack(i+1, result, subset, nums);
+        subset.remove(subset.size() - 1);
+        
+        // EXCLUDE nums[i]
+        backtrack(i+1, result, subset, nums);
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(0, result, new ArrayList<>(), nums);
+        return result;
     }
 }
-
-    
