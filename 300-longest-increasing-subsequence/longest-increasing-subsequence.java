2255577d7e541f8f2d3d846a6ceab4dc9a6d3496
Deena <dynamitebutter137@gmail.com>
2026-02-21 10:58:12 +0530

    Time: 37 ms (74.61%) | Memory: 46.2 MB (55.37%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,29 +1,48 @@
-// Memoization
+// OPTIMIZED - DP
 class Solution {
-    private int f(int i, int prevIndex, int[] nums, int[][] dp){
-        if(i == nums.length){
-            return 0;
-        }
-        if(dp[i][prevIndex + 1] != -1) return dp[i][prevIndex + 1];
-        int notPick = f(i+1, prevIndex, nums, dp);
-        int pick = 0;
-        if(prevIndex == -1 || nums[i] > nums[prevIndex]) {
-            pick = 1 + f(i+1, i, nums, dp);
-        }
-        return dp[i][prevIndex + 1] = Math.max(pick, notPick);
-    }
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        // index -> 0 to n-1
-        // prevIndex -> -1 to n-1 (shift position) => 0 to n
-        int[][] dp = new int[n][n+1];
-        for(int i=0; i<n; i++){
-            Arrays.fill(dp[i], -1);
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, 1);
+        int maxCount = 1;
+        for(int i=1; i<n; i++){
+            for(int prev=0; prev < i; prev++){
+                if(nums[prev] < nums[i]){
+                    dp[i] = Math.max(dp[i], 1 + dp[prev]);
+                }
+            }
+            maxCount = Math.max(maxCount, dp[i]);
         }
-        return f(0, -1, nums, dp);
-
+        return maxCount;
     }
 }
+// Memoization
+// class Solution {
+//     private int f(int i, int prevIndex, int[] nums, int[][] dp){
+//         if(i == nums.length){
+//             return 0;
+//         }
+//         // - If it’s not -1, that means we’ve computed it before.
+//         if(dp[i][prevIndex + 1] != -1) return dp[i][prevIndex + 1];
+//         int notPick = f(i+1, prevIndex, nums, dp);
+//         int pick = 0;
+//         if(prevIndex == -1 || nums[i] > nums[prevIndex]) {
+//             pick = 1 + f(i+1, i, nums, dp);
+//         }
+//         return dp[i][prevIndex + 1] = Math.max(pick, notPick);
+//     }
+//     public int lengthOfLIS(int[] nums) {
+//         int n = nums.length;
+//         // index -> 0 to n-1
+//         // prevIndex -> -1 to n-1 (shift position) => 0 to n
+//         int[][] dp = new int[n][n+1];
+//         for(int i=0; i<n; i++){
+//             Arrays.fill(dp[i], -1);
+//         }
+//         return f(0, -1, nums, dp);
+
+//     }
+// }
 // RECURSION
 // class Solution {
 //     public int f(int i, int prevIndex, int[] nums){
