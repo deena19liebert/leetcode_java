7acd4685d4bf11ff0a943a5157f1ebf80df5caae
Deena <dynamitebutter137@gmail.com>
2026-02-22 12:48:29 +0530

    Time: 1 ms (78.77%) | Memory: 46.7 MB (82.52%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,14 +1,32 @@
+//BETTER APPROACH
 class Solution {
     public int removeDuplicates(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        int j=0;
-        for(int i=0; i<nums.length; i++){
-            if(!set.contains(nums[i])){
-                set.add(nums[i]);
-                nums[j] = nums[i];
-                j++;
+        int n = nums.length;
+        if(n == 0) return 0;
+
+        int left = 0, i = 1;
+        for(int right=0; right<n; right++){
+            if(nums[left] != nums[right]){
+                nums[i] = nums[right];
+                left = right;
+                i++;
             }
         }
-        return set.size();
+        return i;
     }
 }
+//Brute Force
+// class Solution { // TC-0(N), SC-O(N)
+//     public int removeDuplicates(int[] nums) {
+//         Set<Integer> set = new HashSet<>();
+//         int j=0;
+//         for(int i=0; i<nums.length; i++){
+//             if(!set.contains(nums[i])){
+//                 set.add(nums[i]);
+//                 nums[j] = nums[i];
+//                 j++;
+//             }
+//         }
+//         return set.size();
+//     }
+// }
