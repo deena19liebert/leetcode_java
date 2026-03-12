bbeb6e90c52dc9152385fe1054be6b7126bd730f
Deena <dynamitebutter137@gmail.com>
2026-03-12 20:25:24 +0530

    Time: 2 ms (70.56%) | Memory: 55.9 MB (29.78%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,15 +1,31 @@
-class Solution {
+class Solution { // MOORE'S VOTING ALGORITHM
     public int majorityElement(int[] nums) {
-        int ans = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int num : nums){
-            map.put(num, map.getOrDefault(num, 0)+1);
-        }
-        for(int num : nums){
-            if(map.get(num) > nums.length / 2){
-                ans = num;
+        int count=0, num=0;
+        for(int i=0; i<nums.length; i++){
+            if(count == 0){
+                count = 1;
+                num = nums[i];
+            } else if(num == nums[i]){
+                count++;
+            } else {
+                count--;
             }
         }
-        return ans;
+        return num;
     }
 }
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         int ans = 0;
+//         Map<Integer, Integer> map = new HashMap<>();
+//         for(int num : nums){
+//             map.put(num, map.getOrDefault(num, 0)+1);
+//         }
+//         for(int num : nums){
+//             if(map.get(num) > nums.length / 2){
+//                 ans = num;
+//             }
+//         }
+//         return ans;
+//     }
+// }
