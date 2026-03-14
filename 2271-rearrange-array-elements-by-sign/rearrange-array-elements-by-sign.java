f9981ad371b11fae0f3c101d1f386529d8c7d28f
Deena <dynamitebutter137@gmail.com>
2026-03-14 11:32:47 +0530

    Time: 3 ms (99.67%) | Memory: 175.6 MB (54.09%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,24 +1,41 @@
-//BRUTE FORCE, TC: O(N) + O(N), SC: O(N)
+//OPTIMAL SOLUTION, TC: O(N) + O(N), SC: O(N)
 class Solution {
     public int[] rearrangeArray(int[] nums) {
         int n = nums.length;
-        int[] pos = new int[n/2];
-        int[] neg = new int[n/2];
-        int pIndex=0, nIndex=0;
-        for(int i=0; i<n; i++){
+        int[] result = new int[n];
+        int posIndex = 0, negIndex = 0;
+        for (int i=0; i<n; i++){
             if(nums[i] > 0){
-                pos[pIndex] = nums[i]; //STORE ONLY POSITIVE
-                pIndex++; 
+                result[2 * posIndex] = nums[i];
+                posIndex++;
             } else {
-                neg[nIndex] = nums[i]; //STORE ONLY NEGATIVE
-                nIndex++;
+                result[2 * negIndex + 1] = nums[i];
+                negIndex++;
             }
         }
-        // int[] result = new int[n];
-        for(int i=0; i<n/2; i++){ 
-            nums[2 * i] = pos[i]; // even indices (0,2,4) contain positive values
-            nums[2 * i + 1] = neg[i]; // odd indices (1,3,5) contain negative values
-        }
-        return nums;
+        return result;
     }
 }
+//BRUTE FORCE, TC: O(N) + O(N), SC: O(N)
+// class Solution {
+//     public int[] rearrangeArray(int[] nums) {
+//         int n = nums.length;
+//         int[] pos = new int[n/2];
+//         int[] neg = new int[n/2];
+//         int pIndex=0, nIndex=0;
+//         for(int i=0; i<n; i++){
+//             if(nums[i] > 0){
+//                 pos[pIndex] = nums[i]; //STORE ONLY POSITIVE
+//                 pIndex++; 
+//             } else {
+//                 neg[nIndex] = nums[i]; //STORE ONLY NEGATIVE
+//                 nIndex++;
+//             }
+//         }
+//         for(int i=0; i<n/2; i++){ 
+//             nums[2 * i] = pos[i]; // even indices (0,2,4) contain positive values
+//             nums[2 * i + 1] = neg[i]; // odd indices (1,3,5) contain negative values
+//         }
+//         return nums;
+//     }
+// }
