8819a850b1e71acacf93891d51289053b777efc0
Deena <dynamitebutter137@gmail.com>
2026-03-16 09:55:30 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (95.68%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int i = n-2;
+        while(i >= 0 && nums[i] >= nums[i+1]){
+            i--;
+        }
+        if(i >= 0){
+            int j = n-1;
+            while(nums[j] <= nums[i]){
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i+1,n-1);
+    }
+    public void swap(int[] nums, int start, int end){
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+    }
+    public void reverse(int[] nums, int start, int end){
+        while(start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+}
