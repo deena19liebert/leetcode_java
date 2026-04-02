71e4b6f58746fcbbf0a0bfda93cc2b84edbea8ad
Deena <dynamitebutter137@gmail.com>
2026-04-02 11:26:57 +0530

    Time: 2379 ms (5.02%) | Memory: 52.7 MB (46.24%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,23 +1,13 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer, Integer> countElement = new HashMap<>();
-        
-        // For each number in the array, increase its count in the map by 1. If it’s not in the map yet, start its count at 1
-
-        for(int i=0; i < nums.length; i++){
-            countElement.put(nums[i],countElement.getOrDefault(nums[i], 0) + 1);
-        }
-        List<Integer> majorityElements = new ArrayList<>();
-        int threshold = nums.length/3;
-
-        for(Map.Entry<Integer, Integer> entry : countElement.entrySet()) {
-            int element = entry.getKey();
-            int count = entry.getValue();
-            if(count > threshold) {
-                majorityElements.add(element);
+        List<Integer> result = new ArrayList<>();
+        for(int i=0; i<nums.length; i++){
+            int count = 0;
+            for(int j=0; j<nums.length; j++){
+                if(nums[i] == nums[j]) count++;
             }
-
+            if(count > nums.length / 3 && !result.contains(nums[i])) result.add(nums[i]);
         }
-    return majorityElements;
+        return result;
     }
 }
