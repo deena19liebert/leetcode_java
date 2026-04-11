b1381b7569560acf193688b927a50774b131e861
Deena <dynamitebutter137@gmail.com>
2026-04-11 10:37:54 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (32.83%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,10 +1,30 @@
-//BRUTE FORCE
+//OPTIMAL SLUTION / BETTER SLUTION 
+// TC: O(N+M), SC:O(1)
 class Solution {
     public void merge(int[] nums1, int n, int[] nums2, int m) {
-        int[] result = new int[m+n];
-        for(int j=0; j<m; j++){
-            nums1[n+j] = nums2[j];
+        int i = n-1;
+        int j = m-1;
+        int k = n+m-1;
+        while(i >=0 && j >= 0){
+            if(nums1[i] > nums2[j]){
+                nums1[k--] = nums1[i--];
+            }
+            else {
+                nums1[k--] = nums2[j--];
+            }
         }
-        Arrays.sort(nums1);
+        while(j >=0){
+            nums1[k--] = nums2[j--];
+        }
     }
 }
+
+//BRUTE FORCE
+// class Solution {
+//     public void merge(int[] nums1, int n, int[] nums2, int m) {
+//         for(int j=0; j<m; j++){
+//             nums1[n+j] = nums2[j];
+//         }
+//         Arrays.sort(nums1);
+//     }
+// }
