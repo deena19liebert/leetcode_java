bdec6c43b114fece7996c115168d77827ea7283a
Deena <dynamitebutter137@gmail.com>
2026-04-19 15:39:09 +0530

    Time: 1 ms (100.00%) | Memory: 47.3 MB (79.83%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -1,25 +1,30 @@
-// TC: O(n^2) + O(n * n) = O(n^2)+ O(n^2) = O(2n^2) = O(n^2) (remove constant)
-// SC: O(n^2)
+// OPTIMAL SOLUTION, TC: O(n^2), SC: O(1)
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
         int n = grid.length;
-        Map<Integer, Integer> map = new HashMap<>();
+        long N = (long) n * n;
+
+        long expectedSum = N * (N+1) / 2;
+        long expectedSquareSum = N * (N + 1) * (2*N + 1) / 6;
+
+        long actualSum = 0, actualSquareSum = 0;
 
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                int num = grid[i][j];
-                map.put(num, map.getOrDefault(num, 0)+1);
+                long value = grid[i][j];
+                actualSum += value;
+                actualSquareSum += value * value;
             }
         }
-        int duplicate = 0;
-        int missing = 0;
-        for(int i=1; i<=n * n; i++){
-            Integer count = map.get(i);
-            if(count != null && count == 2){
-                duplicate = i;
-            } else if(count == null){
-                missing = i;
-            }
-        } return new int[]{duplicate, missing};
+
+        long diff = actualSum - expectedSum;
+        long diffSquare = actualSquareSum - expectedSquareSum;
+
+        long sumDupMiss = diffSquare / diff;
+
+        int duplicate = (int) ((diff + sumDupMiss)/2);
+        int missing = (int) (sumDupMiss - duplicate);
+
+    return new int[]{duplicate, missing};
     }
 }
