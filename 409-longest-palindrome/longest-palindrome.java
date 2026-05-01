53ae90211f59259dfcb10dcec53f32ec8d9fc080
Deena <dynamitebutter137@gmail.com>
2026-05-01 11:30:09 +0530

    Time: 1 ms (100.00%) | Memory: 43.1 MB (55.28%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.java
+++ 409-longest-palindrome/longest-palindrome.java
@@ -1,38 +1,38 @@
-// BOTH ARE BEST / OPTIMAL
-// class Solution {
-//     public int longestPalindrome(String s) {
-//         int[] count = new int[128]; 
-//         for(char c : s.toCharArray()){
-//             count[c]++;
-//         }
-//         int length = 0;
-//         for(int c : count){
-//             length += (c / 2) * 2;
-//         }
-//         if(s.length() > length){
-//             length += 1;
-//         }
-//         return length;
-//     }
-// }
-// Using hashset
+// OPTIMAL
 class Solution {
     public int longestPalindrome(String s) {
-        Set<Character> set = new HashSet<>(); 
-
-        int length = 0;
-
+        int[] count = new int[128]; 
         for(char c : s.toCharArray()){
-            if(set.contains(c)){
-                length += 2;
-                set.remove(c);
-            } else {
-                set.add(c);
-            }
+            count[c]++;
         }
-        if(!set.isEmpty()){
+        int length = 0;
+        for(int c : count){
+            length += (c / 2) * 2;
+        }
+        if(s.length() > length){
             length += 1;
         }
         return length;
     }
 }
+// Using hashset
+// class Solution {
+//     public int longestPalindrome(String s) {
+//         Set<Character> set = new HashSet<>(); 
+
+//         int length = 0;
+
+//         for(char c : s.toCharArray()){
+//             if(set.contains(c)){
+//                 length += 2;
+//                 set.remove(c);
+//             } else {
+//                 set.add(c);
+//             }
+//         }
+//         if(!set.isEmpty()){
+//             length += 1;
+//         }
+//         return length;
+//     }
+// }
