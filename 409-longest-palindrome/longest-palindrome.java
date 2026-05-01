1d09f203ac125554f6c83f92aa40c672d2590b67
Deena <dynamitebutter137@gmail.com>
2026-05-01 11:29:17 +0530

    Time: 5 ms (56.61%) | Memory: 43.7 MB (7.30%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.java
+++ 409-longest-palindrome/longest-palindrome.java
@@ -1,14 +1,36 @@
+// BOTH ARE BEST / OPTIMAL
+// class Solution {
+//     public int longestPalindrome(String s) {
+//         int[] count = new int[128]; 
+//         for(char c : s.toCharArray()){
+//             count[c]++;
+//         }
+//         int length = 0;
+//         for(int c : count){
+//             length += (c / 2) * 2;
+//         }
+//         if(s.length() > length){
+//             length += 1;
+//         }
+//         return length;
+//     }
+// }
+// Using hashset
 class Solution {
     public int longestPalindrome(String s) {
-        int[] count = new int[128]; 
-        for(char c : s.toCharArray()){
-            count[c]++;
-        }
+        Set<Character> set = new HashSet<>(); 
+
         int length = 0;
-        for(int c : count){
-            length += (c / 2) * 2;
+
+        for(char c : s.toCharArray()){
+            if(set.contains(c)){
+                length += 2;
+                set.remove(c);
+            } else {
+                set.add(c);
+            }
         }
-        if(s.length() > length){
+        if(!set.isEmpty()){
             length += 1;
         }
         return length;
