2dbed4702551cd97056b6012199a0e36b0de570a
Deena <dynamitebutter137@gmail.com>
2026-06-24 23:02:25 +0530

    Time: 7 ms (20.03%) | Memory: 46.7 MB (9.94%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,16 +1,15 @@
 class Solution {
     public int singleNumber(int[] nums) {
-
-        // ones keeps track of bits seen exactly once.
-
-        //twos keeps track of bits seen exactly twice.
-
-        //when a bit is seen a third time, both ones and twos clear it out.
-        int ones = 0, twos = 0;
-        for(int num : nums) {
-            ones = (ones ^ num) & ~twos;
-            twos = (twos ^ num) & ~ones;
+        int ans = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        return ones;
-}
+        for(int num : map.keySet()){
+            if(map.get(num) == 1){
+                ans = num;
+            }
+        }
+        return ans;
+    }
 }
