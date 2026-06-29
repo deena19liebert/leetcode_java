36d12a44b0a7171991d1a10b703ed78f40898386
Deena <dynamitebutter137@gmail.com>
2026-06-29 09:51:40 +0530

    Time: 14 ms (39.09%) | Memory: 46.5 MB (95.96%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,25 +1,22 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> result = new ArrayList<>();
-        if (s.length() < p.length()) return result;
-
         int[] count1 = new int[26];
         int[] count2 = new int[26];
-        
-        for(int i=0; i < p.length(); i++){
-            count1[s.charAt(i) - 'a']++;
-            count2[p.charAt(i) - 'a']++;
-        } 
 
-        if(Arrays.equals(count1, count2)) {
-            result.add(0);
+        List<Integer> result = new ArrayList<>();
+        for(char c : p.toCharArray()){
+            count1[c - 'a']++;
         }
+        //SILDING WINDOW
+        for(int i=0; i<s.length(); i++){
+            count2[s.charAt(i) - 'a']++;
 
-        for(int right=p.length(); right < s.length(); right++){
-            count1[s.charAt(right) - 'a']++;
-            count1[s.charAt(right - p.length()) - 'a']--;
+            // Remove the character out of the window
+            if(i >= p.length()){
+                count2[s.charAt(i - p.length()) - 'a']--;
+            }
             if(Arrays.equals(count1, count2)) {
-                result.add(right - p.length() + 1);
+                result.add(i - p.length() + 1);
             }
         }
         return result;
